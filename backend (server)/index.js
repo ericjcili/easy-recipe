4f465a2371a3bf200632881747be5c2a84229b30
ericjcili <ericjcili@gmai.com>
2021-11-23 21:11:29 -0500

    test3

--- backend (server)/index.js
+++ backend (server)/index.js
@@ -3,8 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import MONGODB_URI from 'dotenv';
+
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
+
 
 const app = express();
 
@@ -15,9 +18,10 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const PORT = process.env.PORT|| 3000;
+const URI = process.env.MONGODB_URI || 'mongodb+srv://dbUser:pasword@cluster0.9cxdz.mongodb.net/recipe-list?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 3001;
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
