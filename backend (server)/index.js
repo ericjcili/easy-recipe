6af12755f74c1a65ff723e06070426225c8768c1
ericjcili <ericjcili@gmai.com>
2021-11-24 01:40:46 -0500

    please

--- backend (server)/index.js
+++ backend (server)/index.js
@@ -8,20 +8,29 @@ import dotenv from 'dotenv/config';
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
 
 const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://dbUser:pasword@cluster0.9cxdz.mongodb.net/recipe-list?retryWrites=true&w=majority';
+const MONGODB_URI = process.env.MONGODB_URI //|| 'mongodb+srv://dbUser:pasword@cluster0.9cxdz.mongodb.net/recipe-list?retryWrites=true&w=majority';
 const PORT = 3001;
 
 console.log(MONGODB_URI);
+console.log(MONGODB_URI);
+console.log(MONGODB_URI);
+console.log(MONGODB_URI);
+
 
 console.log(PORT);
 
