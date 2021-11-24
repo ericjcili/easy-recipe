ac6dd71fc0039d0316b49fb341dcdf7b735d6545
ericjcili <ericjcili@gmai.com>
2021-11-23 23:48:57 -0500

    lord help us

--- backend (server)/index.js
+++ backend (server)/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+const path = import('path');
+import dotenv from 'dotenv/config';
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
@@ -16,14 +18,14 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-var MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://dbUser:pasword@cluster0.9cxdz.mongodb.net/recipe-list?retryWrites=true&w=majority';
-const PORT = 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://dbUser:pasword@cluster0.9cxdz.mongodb.net/recipe-list?retryWrites=true&w=majority';
+const PORT = 3001;
 
 console.log(MONGODB_URI);
 
 console.log(PORT);
 
-mongoose.connect( MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
