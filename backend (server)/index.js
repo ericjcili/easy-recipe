5348aa0d3ce6ca12bcab1f44023ec4808f045966
ericjcili <ericjcili@gmai.com>
2021-11-24 02:14:53 -0500

    please

--- backend (server)/index.js
+++ backend (server)/index.js
@@ -20,14 +20,6 @@ app.use("/user", userRouter);
 const MONGODB_URI = process.env.MONGODB_URI //|| 'mongodb+srv://dbUser:pasword@cluster0.9cxdz.mongodb.net/recipe-list?retryWrites=true&w=majority';
 const PORT = process.env.PORT;
 
-console.log(MONGODB_URI);
-console.log(MONGODB_URI);
-console.log(MONGODB_URI);
-console.log(MONGODB_URI);
-
-
-console.log(PORT);
-
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
